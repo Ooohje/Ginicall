acf100b41c4eb755f6f6a7fde7e89672c681818f
Jeong SuHyeon <jsh00325@naver.com>
2023-11-06 16:46:13 +0900

    Apply Layout

---
+++ app/src/main/java/com/example/ginicall/Shelter/Shelter_Info_Activity.java
@@ -0,0 +1,16 @@
+package com.example.ginicall.Shelter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ginicall.R;
+
+public class Shelter_Info_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shelter_info);
+    }
+}
