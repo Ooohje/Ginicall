a40a0ac06f23ef6fe0c2ab624ea3c7790e92e842
ohje <ohjs87968628@gmail.com>
2023-11-09 15:46:03 +0900

    Warning_Activity&&Button Connect

--- app/src/main/java/com/example/ginicall/Shelter/Shelter_Fragment.java
+++ app/src/main/java/com/example/ginicall/Shelter/Shelter_Fragment.java
@@ -14,10 +14,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.ginicall.R;
+import com.example.ginicall.Warning.Warning_Activity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
@@ -44,6 +46,7 @@ import com.kakao.vectormap.label.Transition;
 
 public class Shelter_Fragment extends Fragment {
     private View view;
+    private Button warningButton;
     private Context context;
     private KakaoMap kakaoMap;
     private LabelLayer labelLayer;
@@ -62,6 +65,7 @@ public class Shelter_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view =  inflater.inflate(R.layout.fragment_shelter_, container, false);
         context = container.getContext();
+        warningButton = view.findViewById(R.id.warning_button);
         MapView mapView = view.findViewById(R.id.shelter_map_view);
         mapView.start(new MapLifeCycleCallback() {
             @Override
@@ -82,6 +86,15 @@ public class Shelter_Fragment extends Fragment {
                 labelLayer = kakaoMap.getLabelManager().getLayer();
                 locateMarker(map);
                 moveCamera(LatLng.from(35.88900, 128.6103));
+            }
+        });
+        warningButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "경고!!! 지진 발생!!!", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(getActivity(), Warning_Activity.class);
+                startActivity(intent);
             }
         });
 
---
+++ app/src/main/java/com/example/ginicall/Warning/Warning_Activity.java
@@ -0,0 +1,16 @@
+package com.example.ginicall.Warning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ginicall.R;
+
+public class Warning_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_warning);
+    }
+}
